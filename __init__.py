39996c3bf5a8bcbb49922dadc96525c4c61ebe52
aleswy0 <aswyst@gmail.com>
2022-05-12 20:53:40 -0400

    comments and cleanup

--- __init__.py
+++ __init__.py
@@ -48,100 +48,141 @@ headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36"
 }
 
-# Update Note Fields
+# Update Note Field after Image Scraping
 def updateField(mw, config, nid, fld, images, overwrite):
-    if not images:
+    if not images: # if no images, exit and return nothing
         return
-    imgs = []
-    for fname, data in images:
+
+    imgs = [] # image fnames for insertion to note
+    for fname, data in images: # for list of (fname,data) tuples, add fnames to 'imgs' list of filenames
         fname = mw.col.media.writeData(fname, data)
         filename = '<img src="%s">' % fname
         imgs.append(filename)
+
     note = mw.col.getNote(nid)
     delimiter = config.get("Delimiter", " ")
+    
     if overwrite == "Append":
         if note[fld]:
             note[fld] += delimiter
         note[fld] += delimiter.join(imgs)
     else:
         note[fld] = delimiter.join(imgs)
+    # END IF
+
     note.flush()
+# END FUNCTION
 
 # Scrape Images from URL
-def getImages(nid, fld, html, img_width, img_height, img_count, fld_overwrite):
+def scrapeImages(nid, fld, html, img_width, img_height, img_count, fld_overwrite):
     from PIL import Image, ImageSequence, UnidentifiedImageError
 
+    # Extract image divs from html
     soup = BeautifulSoup(html, "html.parser")
-    rg_meta = soup.find_all("div", {"class": "rg_meta"})
-    metadata = [json.loads(e.text) for e in rg_meta]
-    results = [d["ou"] for d in metadata]
+    rg_meta = soup.find_all("div", {"class": "rg_meta"}) # collect all div.rg_meta elements from html
+    metadata = [json.loads(e.text) for e in rg_meta] # for each div in rg_meta, convert json to dict, add to list
+    results = [d["ou"] for d in metadata] # take img tags? from list of json, add to results list
 
-    if not results:
-        regex = re.escape("AF_initDataCallback({")
-        regex += r'[^<]*?data:[^<]*?' + r'(\[[^<]+\])'
+    if not results: # if results empty ...
+        regex = re.escape("AF_initDataCallback({") # escape special chars in string
+        regex += r'[^<]*?data:[^<]*?' + r'(\[[^<]+\])' # search for string w/ 'data:'
 
-        for txt in re.findall(regex, html):
-            data = json.loads(txt)
+        for txt in re.findall(regex, html): # for each result string from re.findall ...
+            data = json.loads(txt) # convert json from result to dict
 
             try:
                 for d in data[31][0][12][2]:
                     try:
-                        results.append(d[1][3][0])
+                        results.append(d[1][3][0]) # add URLs to results list
                     except Exception as e:
                         pass
             except Exception as e:
                 pass
+        # END FOR
+    # END IF
 
     cnt = 0
     images = []
-    for url in results:
+    for url in results: # for each url in results list
         try:
-            r = requests.get(url, headers=headers, timeout=15)
+            r = requests.get(url, headers=headers, timeout=15) #HTTP GET Request, 'r'
             r.raise_for_status()
-            data = r.content
+            data = r.content # image file?
+
+            # Skip if doc header is text or svg
             if 'text/html' in r.headers.get('content-type', ''):
                 continue
+            # END IF
             if 'image/svg+xml' in r.headers.get('content-type', ''):
                 continue
-            url = re.sub(r"\?.*?$", "", url)
-            path = urllib.parse.unquote(url)
-            fname = os.path.basename(path)
+            # END IF
+
+            url = re.sub(r"\?.*?$", "", url) # get text after '?' in request url for filename
+            path = urllib.parse.unquote(url) # parse url
+            fname = os.path.basename(path) # set filename
+
             if not fname:
                 fname = checksum(data)
-            im = Image.open(io.BytesIO(data))
+            # END IF
+            
+            # == IMAGE PROCESSING ==
+            im = Image.open(io.BytesIO(data)) # stream image data and open
+
             if img_width > 0 or img_height > 0:
                 width, height = im.width, im.height
+
                 if img_width > 0:
-                    width = min(width, img_width)
+                    width = min(width, img_width) # calc img width from min of query width or actual width
+                # END IF
+
                 if img_height > 0:
-                    height = min(height, img_height)
+                    height = min(height, img_height) # ditto for height
+                # END IF
+
                 buf = io.BytesIO()
-                if getattr(im, 'n_frames', 1) == 1:
-                    im.thumbnail((width, height))
-                    im.save(buf, format=im.format, optimize=True)
-                elif mpv_executable:
+
+                if getattr(im, 'n_frames', 1) == 1: # detect static img or gif by frame count
+                    im.thumbnail((width, height)) # set image dims
+                    im.save(buf, format=im.format, optimize=True) # save img
+
+                # Gif handling?
+                elif mpv_executable: # if gif
                     thread_id = threading.get_native_id()
                     tmp_path = tmpfile(suffix='.{}'.format(thread_id))
                     with open(tmp_path, 'wb') as f:
                         f.write(data)
+
                     img_fmt = im.format.lower()
                     img_ext = '.' + img_fmt
                     img_path = tmpfile(suffix=img_ext)
                     cmd = [mpv_executable, tmp_path, "-vf", "lavfi=[scale='min({},iw)':'min({},ih)':force_original_aspect_ratio=decrease:flags=lanczos]".format(img_width, img_height), "-o", img_path]
+
                     with noBundledLibs():
                         p = subprocess.Popen(cmd, startupinfo=si, stdin=subprocess.PIPE,
                             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
                             env=env)
+                    # END WITH
+
                     if p.wait() == 0:
                         with open(img_path, 'rb') as f:
                             buf.write(f.read())
+                    # END IF
                 else:
                     buf = io.BytesIO(data)
-                data = buf.getvalue()
-            images.append((fname, data))
+                # END IF
+
+                data = buf.getvalue() # set data to bytes
+            # END IF
+
+            images.append((fname, data)) # add image data to images list as tuple
             cnt += 1
+
             if cnt == img_count:
                 break
+            # END IF
+        # END TRY
+
+        # Error Handling
         except requests.packages.urllib3.exceptions.LocationParseError: # fix from anonymous @ https://ankiweb.net/shared/info/561924305
             pass
         except requests.exceptions.RequestException:
@@ -153,7 +194,13 @@ def getImages(nid, fld, html, img_width, img_height, img_count, fld_overwrite):
             # https://bugs.python.org/issue32958
             if str(e) != "encoding with 'idna' codec failed (UnicodeError: label empty or too long)":
                 raise
+            # END IF
+        # END EXCEPT
+
+    # END FOR
+    
     return (nid, fld, images, fld_overwrite)
+# END FUNCTION
 
 # Run query, add images to notes
 def updateNotes(browser, mw, nids, sf, sq, config):
@@ -168,68 +215,81 @@ def updateNotes(browser, mw, nids, sf, sq, config):
 
             w = note[sf] #source field content
 
-            # for each query from form ...
-            for q in sq:
-                df = q["Field"]
+            for q in sq: # for each query from form ...
+                df = q["Field"] # destination field, e.g. {{img}}
 
-                if not df:      # if destination field empty, go to next note ID
+                # Skip conditions
+                if not df:      # if destination form field empty, skip to next note ID
                     continue
-
                 if note[df] and q["Overwrite"] == "Skip":   # if skip selected, go to next note ID
                     continue
                 
-                # Strip HTML from source field
-                w = re.sub(r'</?(b|i|u|strong|span)(?: [^>]+)>', '', w)
+                # == GENERAL FORMATTING OF SOURCE FIELD TEXT ==
+                # Strip HTML from note source field
+                w = re.sub(r'</?(b|i|u|strong|span)(?: [^>]+)>', '', w) # remove basic Anki formatting
                 w = re.sub(r'\[sound:.*?\]', '', w)
-                if '<' in w:
+                if '<' in w: # if left element bracket, '<', still in field text... why?
                     soup = BeautifulSoup(w, "html.parser")
-                    for s in soup.stripped_strings:
+                    for s in soup.stripped_strings: # extract strings from html and strip whitespace
                         w = s
-                        break
+                        break # take only first string from source field...why?
                 else:
                     w = re.sub(r'<br ?/?>[\s\S]+$', ' ', w)
                     w = re.sub(r'<[^>]+>', '', w)
+                # END IF
 
                 # Remove and Reformat Clozes
                 clozes = re.findall(r'{{c\d+::(.*?)(?::.*?)?}}', w)
                 if clozes:
                     w = ' '.join(clozes)
+                # END IF
 
-                query = q["URL"].replace("{}", w) # insert source text into URL
+                # == CREATE & EXECUTE GET REQ ==
+                query = q["URL"].replace("{}", w) # insert field text into query form field text
 
                 # Add requests and scrape to queue list
                 try:
-                    r = requests.get("https://www.google.com/search?tbm=isch&q={}&safe=active".format(query), headers=headers, timeout=15)
-                    r.raise_for_status()
-                    future = executor.submit(getImages, nid, df, r.text, q["Width"], q["Height"], q["Count"], q["Overwrite"])
-                    jobs.append(future)
+                    r = requests.get("https://www.google.com/search?tbm=isch&q={}&safe=active".format(query), headers=headers, timeout=15) # replace '{}' with formatted query, perform GET request
+                    r.raise_for_status() # raise error if request failure
+                    html = r.text
+
+                    future = executor.submit(scrapeImages, nid, df, html, q["Width"], q["Height"], q["Count"], q["Overwrite"]) # async execution of 'getImages'
+                    jobs.append(future) # add to list of jobs
+
                 except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as e:
                     pass
+            # END FOR
 
-            done, not_done = concurrent.futures.wait(jobs, timeout=0)
-            for future in done:
+            done, not_done = concurrent.futures.wait(jobs, timeout=0) # wait for jobs' completion, then add list of outputs to list
+            for future in done: # loop through currently completed jobs, add to 'done' list, remove from 'jobs'
                 nid, fld, images, overwrite = future.result()
                 updateField(mw, config, nid, fld, images, overwrite)
+                
+                # Status Bar
                 processed.add(nid)
                 jobs.remove(future)
-            else:
+            else: # triggers at end of loop if no "break"
                 label = 'Processing ' + str(len(processed)) + ' of ' + str(len(nids))
                 mw.taskman.run_on_main(lambda:mw.progress.update(label=label,value=len(processed),max=len(nids)))
+        # END FOR
 
-        # Execute queued jobs
-        for future in concurrent.futures.as_completed(jobs):
-            nid, fld, images, overwrite = future.result()
+        for future in concurrent.futures.as_completed(jobs): # run loop as jobs complete
+            nid, fld, images, overwrite = future.result() # images is tuple of (fname,data)
             updateField(mw, config, nid, fld, images, overwrite)
+            
+            # Status Bar
             processed.add(nid)
             label = 'Processing ' + str(len(processed)) + ' of ' + str(len(nids))
             mw.taskman.run_on_main(lambda:mw.progress.update(label=label,value=len(processed),max=len(nids)))
+        # END FOR
 
     browser.model.endReset()
     mw.requireReset()
+# END FUNCTION
 
 # Query Form UI
 def updateNotesUI(browser, nids):
-    from PIL import Image, ImageSequence, UnidentifiedImageError
+    # unused: from PIL import Image, ImageSequence, UnidentifiedImageError
 
     mw = browser.mw
 
@@ -393,7 +453,6 @@ def onAddImages(browser):
         return
     updateNotesUI(browser, nids)
     # browser.mw.taskman.with_progress(label='Processing...!',immediate=True, task=lambda: updateNotes(browser, nids),on_done=lambda: showInfo('Processed.', parent=browser))
-
 
 def setupMenu(browser):
     menu = browser.form.menuEdit
